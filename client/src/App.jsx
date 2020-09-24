a17e82839bc5e4fb68291cfa6204b39178e418de
Chris Williams <Chris@Chriss-MacBook-Pro-2.local>
2020-09-24 08:19:54 -0600

    configures circleci

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,15 @@
 import React from 'react';
+import axios from 'axios';
+
+// const apiTest = () => {
+//  axios.get('http://52.26.193.201:3000/products/list')
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   })
+// }
 
 const App = () => (
   <div>
